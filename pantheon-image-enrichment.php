e2dade8cd2e9e9f3e7faf1fe878e9ad92bbbb30f
Daniel Bachhuber <daniel@bachhuber.co>
2018-05-24 04:40:37 -0700

    Initial commit

---
+++ pantheon-image-enrichment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Pantheon Image Enrichment
+ * Plugin URI:      https://pantheon.io
+ * Description:     Generate default alt text, auto-crop and more.
+ * Author:          Pantheon
+ * Author URI:      https://pantheon.io
+ * Text Domain:     pantheon-image-enrichment
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Pantheon_Image_Enrichment
+ */
+
+// Your code starts here.
